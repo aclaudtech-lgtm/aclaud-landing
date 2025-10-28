20bc014debf3c9fb0ce0e84b65d444e1a9f83dbb
Ashish Sharma Param Sharma <sharma@mac.lan>
2025-10-28 10:52:16 -0400

    Initial commit of jsx pages

---
+++ src/index.js
@@ -0,0 +1,8 @@
+    // index.js
+    import React from 'react';
+    import ReactDOM from 'react-dom/client'; // Use createRoot for React 18+
+    import App from './App.jsx';
+
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render('<React.StrictMode>\r\n            <App \/>\r\n        <\/React.StrictMode>');
+
